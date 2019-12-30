2fe85a14359559d4389f8e3db3f3cd37bc8e3258
Johannes Aigner <aigner.jo@web.de>
2019-12-30 23:23:13 +0100

    Feature/entsoe graphql 3 (#4)
    * Fixed some failing tests
    
    * Authorization header is called "securityToken" in Entsoe API
    * Entsoe API response is in xml format -> fixed accordingly
    
    * Added Coverage
    
    * Added coverage for unit tests
    * Todo: Testing 🤟
    
    * Added Tests: DayToRange, ExceptionHandling
    
    * added /reports to gitignore
    * added unit tests to match coverage
    
    * Added Licence
    
    * entsoe-graphql-3 GraphQL Testing
    
    * Added Tests for AxiosFactory
    * Added Tests for EntsoeService
    * Added HttpServiceMock
    * Added Tick mock
    
    * Added Dockerfile
    * Added .dockerignore
    
    * entsoe-graphql-3 GraphQL Testing
    
    * Added Tests for Entsoe Service
    * Added mock data

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,23 +1,24 @@
+import { HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+
 import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app;
+    let app;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    it('/ (GET)', () => {
+        return request(app.getHttpServer())
+            .get('/health')
+            .expect(HttpStatus.OK);
+    });
 });
